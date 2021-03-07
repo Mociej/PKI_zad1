f796853cab5ff63de9f7add929cb259a42bb3ef1
Mociej <maciekkocemba@gmail.com>
2021-03-07 14:33:57 +0100

    Revert "dodanie branch 03 i zmiana main"
    This reverts commit cef5c37899ff37992ae406503c6e62289b62d80f.

--- src/PKI_zad1/Main.java
+++ src/PKI_zad1/Main.java
@@ -3,13 +3,6 @@ package PKI_zad1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
-        System.out.println("nowy branch 03");
+	// write your code here
     }
 }
