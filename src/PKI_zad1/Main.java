8a675a32dfcd8c56995aa5a37d1f6c061863f3c3
Maciej Kocemba <maciekkocemba@gmail.com>
2021-03-07 13:30:26 +0100

    dodanie dwóch println

--- src/PKI_zad1/Main.java
+++ src/PKI_zad1/Main.java
@@ -3,6 +3,8 @@ package PKI_zad1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Branch 02");
+        System.out.println("Branch 02");
+        System.out.println("Branch 02");
     }
 }
